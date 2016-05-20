d1063042eb346a9946da442237bc645b12db1347
Kimmy Andersson <cyaspik@gmail.com>
2016-05-20 23:57:30 +0200

    Implemented MvcCodeRouting and set preCondition for the persistence module so that it is only used when a managed request is served.

--- BloodCore.App/App_Start/RouteConfig.cs
+++ BloodCore.App/App_Start/RouteConfig.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using MvcCodeRouting;
 using System.Web.Mvc;
 using System.Web.Routing;
 
@@ -13,11 +10,7 @@ namespace BloodCore.App
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            routes.MapCodeRoutes(typeof(Controllers.HomeController));
         }
     }
 }
