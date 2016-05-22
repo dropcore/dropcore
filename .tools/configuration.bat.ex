074f33edf3cc1ee6663fb4390b1837b25cce2ecc
Kimmy Andersson <cyaspik@gmail.com>
2016-05-22 17:44:36 +0200

    Ignore migration configuration.

--- .tools/configuration.bat.ex
+++ .tools/configuration.bat.ex
@@ -3,4 +3,4 @@
 SET BuildMode=Debug
 SET FluentMigratorToolsVersion=1.6.1
 SET DatabaseProvider=MySql
-SET ConnectionString="Server=localhost;Database=bloodcore_dev;Uid=root;Pwd=root;"
+SET ConnectionString=""
